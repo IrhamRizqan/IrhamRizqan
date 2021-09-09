fc0e237a083930b4d7a95333f35e22b1f270ce66
IrhamR <59431897+IrhamRizqan@users.noreply.github.com>
2021-09-09 11:25:58 +0700

    Add files via upload
    first learn pascal from high school

---
+++ main.pas
@@ -0,0 +1,27 @@
+{
+
+                            Online Pascal Compiler.
+                Code, Compile, Run and Debug Pascal program online.
+Write your code in this editor and press "Run" button to execute it.
+
+}
+
+
+program hewan;
+uses crt;
+var
+namaHewan:string;
+
+
+begin
+  
+  writeln('Nama Hewan:');
+  read(namaHewan);
+  
+  
+  writeln('Jenis Hewan: Mamalia');
+  writeln('Kategori Hewan: Herbivora');
+  writeln('Berwarna: Abu-abu');
+
+end.
+
