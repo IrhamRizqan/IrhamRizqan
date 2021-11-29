e45fe42913ba837ee7a62fe24c38b10703952ee2
IrhamR <59431897+IrhamRizqan@users.noreply.github.com>
2021-11-29 13:16:26 +0700

    -

---
+++ hari + bulan.pas
@@ -0,0 +1,59 @@
+program Hello;
+uses crt;
+var hari,bulan,hasil:integer;
+
+begin
+clrscr;
+write('Masukan hari ke : ');
+readln(hari);
+write('Masukan bulan ke : ');
+readln(bulan);
+
+if (bulan = 1) then
+hasil := 31+hari;
+writeln(hasil)
+
+else if (bulan = 2) then
+hasil := 31+hari;
+writeln(hasil);
+
+else if (bulan = 3) then
+hasil := 31+hari;
+writeln(hasil);
+
+else if (bulan = 4) then
+hasil := 31+hari;
+writeln(hasil);
+
+else if (bulan = 5) then
+hasil := 31+hari;
+writeln(hasil);
+
+else if (bulan = 6) then
+hasil := 31+hari;
+writeln(hasil);
+
+else if (bulan = 7) then
+hasil := 31+hari;
+writeln(hasil);
+
+else if (bulan = 8) then
+hasil := 31+hari;
+writeln(hasil);
+
+else if (bulan = 9) then
+hasil := 31+hari;
+writeln(hasil);
+
+else if (bulan = 10) then
+hasil := 31+hari;
+writeln(hasil);
+
+else if (bulan = 11) then
+hasil := 31+hari;
+writeln(hasil);
+
+else if (bulan = 12) then
+hasil := 31+hari;
+writeln(hasil);
+end.
